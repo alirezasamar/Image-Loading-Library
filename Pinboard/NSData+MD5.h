0c203a647d1852aa37ca6fcf78fb42cb6e8f9967
Alireza Samar <alireza.smr@gmail.com>
2015-09-15 16:30:58 +0800

    MD5 Added

---
+++ Pinboard/NSData+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSData+MD5.h
+//  Pinboard
+//
+//  Created by Alireza Samar on 9/15/15.
+//  Copyright (c) 2015 Alireza Samar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+
+- (NSString*)MD5;
+
+@end
